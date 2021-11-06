3c13c9507f9e23fd4de9655a8a169d170d77c6a4
Faruq <faruqabdulsalam@yahoo.com>
2021-11-06 09:39:57 +0100

    Create API fetch loading component

---
+++ frontend/src/components/Loading.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Loading(Component) {
+  return function Loadng({ isLoading, ...props }) {
+    if (!isLoading) return <Component {...props} />;
+    return (
+      <p style={{ textAlign: 'center', fontSize: '30px' }}>
+        Hold on, fetching data!!
+        This may take some time :)
+      </p>
+    );
+  };
+}
+export default Loading;
