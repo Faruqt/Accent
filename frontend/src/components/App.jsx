ce3cba736b127eea3641d963d286586387876d2c
Faruq <faruqabdulsalam@yahoo.com>
2021-11-06 10:11:10 +0100

    Remove Loading and Accent components from App

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -1,38 +1,15 @@
-import React, { useEffect, useState } from 'react';
-import axios from "axios";
+import React from 'react';
 import Header from "./Header"
 import Search from "./Search"
 import Footer from "./Footer"
 
 function App() {
     
-  const [appState, setAppState] = useState({
-    loading: false,
-    agents: null,
-    fee: null,
-  });
-
-  let one = "/api/properties/"
-  let two = "/api/grandtotal/"
-
-  const requestOne = axios.get(one);
-  const requestTwo = axios.get(two);
-
-  useEffect(() => {
-    setAppState({ loading: true });
-      axios.all([requestOne, requestTwo]).then(axios.spread((...agents) => {
-          const [allEstates, total] = agents
-          setAppState({ loading: false, agents: allEstates.data, fee: total.data});
-        })).catch((err) => console.log(err));
-
-      // eslint-disable-next-line react-hooks/exhaustive-deps
-      }, [setAppState]);
-
   return (
     <div className='App'>
       <Header/>
-      <Search isLoading={appState.loading} agentz={appState.agents} fees={appState.fee}/>
-       <Footer />
+      <Search />
+      <Footer />
     </div>
   );
 }
